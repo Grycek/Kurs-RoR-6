9956ab416e1984e8016ebcfd1022c8c63294edbd
Grycek <adam.grycner@wp.pl>
2010-11-16 14:46:59 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Task6::Application.config.secret_token = '90a6742ceb75f676a8908de56fca5c5054c5c7188de5024057b8ab8fbaf367407bff8a3a5405b22c53a23c26bedabaff6af87e201627aa655ce3362456fa025b'
